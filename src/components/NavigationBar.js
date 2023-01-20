747d10aa510a92c0b5c3c0d6852cc34da829e3f5
Scynes <dustin.greyfield@gmail.com>
2023-01-20 11:30:34 -0500

    complete: added join nav

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,7 @@ const NavigationBar = (props) => {
     return (
         <nav className="bg-color-p">
             <NavLink to='/' className="text-center flex-center">Home</NavLink>
-            <NavLink to='/about' className="text-center flex-center">About</NavLink>
+            <NavLink to='/join' className="text-center flex-center">Join</NavLink>
             <h1 className="flex-center color-p">Ghost Games</h1>
             <NavLink to='/login' className="text-center flex-center">Login</NavLink>
         </nav>

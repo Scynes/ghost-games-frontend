2b6af6dd4f19c55f77ab2e209b48f3ffbcc13aaf
Scynes <dustin.greyfield@gmail.com>
2023-01-21 12:50:37 -0500

    tweak: disabled autocomplete for join form

--- src/components/game/join.js
+++ src/components/game/join.js
@@ -13,7 +13,7 @@ const Join = (props) => {
     return (
         <fieldset className="border-radius-10px">
             <legend className="text-center"><h2>Join Game</h2></legend>
-            <form id="join-game" className="flex-center flex-column" onSubmit={handleSubmit}>
+            <form id="join-game" className="flex-center flex-column" onSubmit={handleSubmit} autoComplete="off">
                 <label className="align-self-flex-start" htmlFor="room-code">Room</label>
                 <input id="room-code" name="room" className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="text" placeholder="ENTER ROOM CODE"/>
                 <label className="align-self-flex-start" htmlFor="display-name">Display Name</label>

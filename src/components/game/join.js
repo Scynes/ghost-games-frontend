57837b87cceab60fa25d743ddc15e5585487cecf
Scynes <dustin.greyfield@gmail.com>
2023-01-21 18:31:07 -0500

    complete: login/signup form (not functional yet)

--- src/components/game/join.js
+++ src/components/game/join.js
@@ -11,16 +11,18 @@ const Join = (props) => {
     }
 
     return (
-        <fieldset className="border-radius-10px">
-            <legend className="text-center"><h2>Join Game</h2></legend>
-            <form id="join-game" className="flex-center flex-column" onSubmit={handleSubmit} autoComplete="off">
-                <label className="align-self-flex-start" htmlFor="room-code">Room</label>
-                <input id="room-code" name="room" className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="text" placeholder="ENTER ROOM CODE"/>
-                <label className="align-self-flex-start" htmlFor="display-name">Display Name</label>
-                <input id="display-name" name="display" className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="text" placeholder="DISPLAY NAME"/>
-                <input className="borderless border-radius-5px bg-color-t color-t" type="submit" value="Join Game!"/>
-            </form>
-        </fieldset>
+        <div id="join-game-wrapper">
+            <fieldset className="border-radius-10px trans-shadowcast">
+                <legend className="text-center"><h2>Join Game</h2></legend>
+                <form id="join-game" className="flex-center flex-column" onSubmit={handleSubmit} autoComplete="off">
+                    <label className="align-self-flex-start" htmlFor="room-code">Room</label>
+                    <input id="room-code" name="room" className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="text" placeholder="ENTER ROOM CODE"/>
+                    <label className="align-self-flex-start" htmlFor="display-name">Display Name</label>
+                    <input id="display-name" name="display" className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="text" placeholder="DISPLAY NAME"/>
+                    <input className="borderless border-radius-5px bg-color-t color-t" type="submit" value="Join Game!"/>
+                </form>
+            </fieldset>
+        </div>
     );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 const Login = () => {
 
+    const [loginHidden, setLoginHidden] = useState(false);
+
+    const CONTAINER_CLASSES = ['flex-center', 'flex-column', 'height-100', 'pad-20'];
+
+    const showSignup = () => {
+
+        setLoginHidden(!loginHidden)
+    }
     return (
-        <div>
-            
+        <div id="login-signup-wrapper" className="border-radius-10px trans-shadowcast">
+            <div id="login-container" className={loginHidden ? CONTAINER_CLASSES.join(' ') + ' hidden' : CONTAINER_CLASSES.join(' ')}>
+                <form>
+                    <input className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="text" name="username" id="username" placeholder="Username"/>
+                    <input className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="password" name="password" id="password" placeholder="Password"/>
+                    <input className="borderless border-radius-5px bg-color-t color-t" type="submit" value="Submit" />
+                    <p className="text-center">OR</p>
+                </form>
+                <input className="borderless border-radius-5px bg-color-t color-t" type="button" value="Signup" onClick={() => showSignup()}/>
+            </div>
+            <div id="signup-container" className={loginHidden ? CONTAINER_CLASSES.join(' ') + ' active' : CONTAINER_CLASSES.join(' ')}>
+                <form>
+                    <input className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="text" name="username" id="s-username" placeholder="Username"/>
+                    <input className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="password" name="password" id="s-password" placeholder="Password"/>
+                    <input className="borderless border-radius-5px bg-color-p tertiary-outline text-center" type="password" name="password" id="s-cpassword" placeholder="Confirm Password"/>
+                    <input className="borderless border-radius-5px bg-color-t color-t" type="submit" value="Submit" />
+                    <p className="text-center">OR</p>
+                </form>
+                <input className="borderless border-radius-5px bg-color-t color-t" type="button" value="Signup" onClick={() => showSignup()}/>
+            </div>
         </div>
     );
 }
